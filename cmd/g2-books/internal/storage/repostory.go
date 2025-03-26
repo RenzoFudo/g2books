99e2466c7d18cf50d65c3246cb7520c709d84ee8
RenzoFudo <143289494+RenzoFudo@users.noreply.github.com>
2025-03-26 14:08:56 +0500

    4
    4

---
+++ cmd/g2-books/internal/storage/repostory.go
@@ -0,0 +1,117 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/RenzoFudo/g2books/cmd/g2-books/internal/domain/models"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
+)
+
+const ctxTimeout = time.Second * 2
+
+type Repository struct {
+	conn *pgxpool.Pool
+}
+
+func NewRepo(ctx context.Context, dbAddr string) (*Repository, error) {
+	conn, err := pgxpool.New(ctx, dbAddr)
+	if err != nil {
+		return nil, err
+	}
+	return &Repository{
+		conn: conn,
+	}, nil
+
+}
+
+func (r *Repository) SaveUser(user models.User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+	_, err := r.conn.Exec(ctx, "INSERT INTO Users (UID, Name, Email, Pass) VALUES ($1, $2, $3, $4)",
+		uuid.New().String(), user.Name, user.Email, user.Pass)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (r *Repository) ValidateUser(user models.User) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+	row := r.conn.QueryRow(ctx, "SELECT Pass FROM Users WHERE Email = $1", user.Email)
+	var pass string
+	if err := row.Scan(&pass); err != nil {
+		return "", err
+	}
+	return pass, nil
+
+}
+
+func (r *Repository) GetBooks() ([]models.Book, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+	rows, err := r.conn.Query(ctx, "SELECT * FROM Books")
+	if err != nil {
+		return nil, err
+	}
+	var books []models.Book
+	for rows.Next() {
+		var book models.Book
+		if err := rows.Scan(&book.BID, &book.Lable, &book.Author); err != nil {
+			return nil, err
+
+		}
+		books = append(books, book)
+	}
+	if len(books) == 0 {
+		return nil, errors.New("no books in database")
+	}
+	return books, nil
+}
+
+func (r *Repository) GetBookById(BID string) (models.Book, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+	row := r.conn.QueryRow(ctx, "SELECT Lable, Author FROM Books WHERE BID = $1", BID)
+	var book models.Book
+	if err := row.Scan(&book.Lable, &book.Author); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return models.Book{}, fmt.Errorf("book %s not found", BID)
+		}
+		return models.Book{}, err
+	}
+	return book, nil
+
+}
+func (r *Repository) SaveBook(book models.Book) error {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+	_, err := r.conn.Exec(ctx, "INSERT INTO Books (BID,Lable, Author) VALUES ($1, $2, $3)",
+		uuid.New().String(), book.Lable, book.Author)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (r *Repository) DeleteBook(BID string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
+	defer cancel()
+	tx, err := r.conn.Begin(ctx)
+	if err != nil {
+		return err
+
+	}
+	defer tx.Rollback(ctx)
+
+	if _, err := tx.Prepare(ctx, "deleteBook", "DELETE FROM Books WHERE BID = $1"); err != nil {
+		return err
+	}
+	_, err = tx.Exec(ctx, "deleteBook", BID)
+	if err != nil {
+		return err
+	}
+	return tx.Commit(ctx)
+}
